c57117cffdd5c83e962745d1d90ee77df3098802
Abhik <theabhik2020@gmail.com>
2025-04-28 12:10:26 +0530

    first commit

---
+++ firstP/src/main/java/com/example/patra/avik_part1/firstP/FirstPApplication.java
@@ -0,0 +1,13 @@
+package com.example.patra.avik_part1.firstP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstPApplication.class, args);
+	}
+
+}
---
+++ firstP/src/test/java/com/example/patra/avik_part1/firstP/FirstPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patra.avik_part1.firstP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
